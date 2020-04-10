608a672ba422fbe50d881971f2587958b112d8cd
L TW <lwat413@gmail.com>
2020-04-10 12:22:02 -0500

    Done

--- blueBrowser/Form1.Designer.cs
+++ blueBrowser/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace blueBrowser
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.frmMain = new System.Windows.Forms.WebBrowser();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
             this.btnBack = new System.Windows.Forms.ToolStripButton();
             this.btnForward = new System.Windows.Forms.ToolStripButton();
             this.cboLocation = new System.Windows.Forms.ToolStripComboBox();
             this.btnGo = new System.Windows.Forms.ToolStripButton();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,18 +49,21 @@
             this.btnGo});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(644, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(504, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // frmMain
+            // webBrowser
             // 
-            this.frmMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.frmMain.Location = new System.Drawing.Point(0, 25);
-            this.frmMain.MinimumSize = new System.Drawing.Size(20, 20);
-            this.frmMain.Name = "frmMain";
-            this.frmMain.Size = new System.Drawing.Size(644, 348);
-            this.frmMain.TabIndex = 1;
+            this.webBrowser.ContextMenuStrip = this.contextMenuStrip1;
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser.Location = new System.Drawing.Point(0, 25);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(504, 348);
+            this.webBrowser.TabIndex = 1;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
             // 
             // btnBack
             // 
@@ -68,6 +73,7 @@
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(36, 22);
             this.btnBack.Text = "Back";
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // btnForward
             // 
@@ -77,11 +83,14 @@
             this.btnForward.Name = "btnForward";
             this.btnForward.Size = new System.Drawing.Size(54, 22);
             this.btnForward.Text = "Forward";
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
             // 
             // cboLocation
             // 
+            this.cboLocation.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
             this.cboLocation.Name = "cboLocation";
             this.cboLocation.Size = new System.Drawing.Size(121, 25);
+            this.cboLocation.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cboLocation_KeyDown);
             // 
             // btnGo
             // 
@@ -91,15 +100,21 @@
             this.btnGo.Name = "btnGo";
             this.btnGo.Size = new System.Drawing.Size(26, 22);
             this.btnGo.Text = "Go";
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
             // 
-            // Form1
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 373);
-            this.Controls.Add(this.frmMain);
+            this.ClientSize = new System.Drawing.Size(504, 373);
+            this.Controls.Add(this.webBrowser);
             this.Controls.Add(this.toolStrip1);
-            this.Name = "Form1";
+            this.Name = "frmMain";
             this.Text = "Blue Browser";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
@@ -111,11 +126,12 @@
         #endregion
 
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.WebBrowser frmMain;
+        private System.Windows.Forms.WebBrowser webBrowser;
         private System.Windows.Forms.ToolStripButton btnBack;
         private System.Windows.Forms.ToolStripButton btnForward;
         private System.Windows.Forms.ToolStripComboBox cboLocation;
         private System.Windows.Forms.ToolStripButton btnGo;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
--- blueBrowser/Form1.cs
+++ blueBrowser/Form1.cs
@@ -10,11 +10,39 @@ using System.Windows.Forms;
 
 namespace blueBrowser
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void cboLocation_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                webBrowser.Navigate(cboLocation.Text);
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate(cboLocation.Text);
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Text = "blueBrowser - " + webBrowser.Document.Title;
         }
     }
 }
